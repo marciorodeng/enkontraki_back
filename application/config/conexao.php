56a8166e823eeaa12216f4f25623b473d5554f55
marciorodeng <marciorodeng@gmail.com>
2021-07-20 08:44:30 -0300

    caminho do banco de dados

--- application/config/conexao.php
+++ application/config/conexao.php
@@ -4,23 +4,24 @@
 	$servidor = "159.89.138.173";
 	$usuario = "usuario";
 	$senha = "20UtpJ15";
-	$sistema = "sistematestes3";
+	//$sistema = "sistematestes3";
 */	
 	// ""Servidor Local""
 	$servidor = "localhost";
 	$usuario = "root";
 	$senha = "";
-	$sistema = "sistemaantigo";
+	//$sistema = "sistemaantigo";
 	//$sistema = "sistematestes3";	
 	
 	// ""Ambiente de Testes"" 
-	$dbname = "app.testes3";
-	$sandbox = true;	
+	//$dbname = "app.testes3";
+	//$sandbox = true;	
 	
 	// ""Ambiente de Produção""
-	//$dbname = "app";
-	//$sistema = "sistema";
-	//$sandbox = false;
+	$dbname = "app";
+	$sistema = "sistema";
+	//$sandbox = true;
+	$sandbox = false;
 	
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 	$pdo = new PDO('mysql:host=' . $servidor . ';dbname=' . $dbname . ';', $usuario, $senha);
